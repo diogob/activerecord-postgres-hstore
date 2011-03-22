4fc5c69c6b1c7ba4b46c2c444749a66b0720a834
Aleksandar Simic <asimic@gmail.com>
2011-03-22 03:05:29 +0000

    added specs for validation

--- spec/activerecord-postgres-hstore_spec.rb
+++ spec/activerecord-postgres-hstore_spec.rb
@@ -25,5 +25,25 @@ describe "ActiverecordPostgresHstore" do
   it "should convert empty string" do
     ''.from_hstore.should == {}
   end
-  
+
+  context "validation" do
+
+    it "should pass for a blank string" do
+      "".valid_hstore?.should == true
+    end
+
+    it "should pass for double quoted hstore format" do
+      %("foo"=>"bar").valid_hstore?.should == true
+    end
+
+    it "should pass for single quoted hstore format" do
+      %('foo'=>'bar').valid_hstore?.should == true
+    end
+
+    it "should pass for single quoted hstore format" do
+      %(:foo =>'bar').valid_hstore?.should == true
+    end
+
+  end
+
 end
