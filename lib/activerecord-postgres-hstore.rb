7bc55ae7626d1105cbea67801b4d2e72a5390ae7
Baris Gumustas <barisgumustas@gmail.com>
2012-07-09 14:41:29 +0300

    added active-record initializer for non-rails environments

--- lib/activerecord-postgres-hstore.rb
+++ lib/activerecord-postgres-hstore.rb
@@ -1,5 +1,9 @@
 if defined? Rails
   require "activerecord-postgres-hstore/railties"
+else
+  ActiveSupport.on_load :active_record do
+    require "activerecord-postgres-hstore/activerecord"
+  end
 end
 require "activerecord-postgres-hstore/string"
 require "activerecord-postgres-hstore/hash"
