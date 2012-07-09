9b46814116433e594297ecdcf19f79322f266fab
Baris Gumustas <barisgumustas@gmail.com>
2012-07-09 14:38:19 +0300

    added support for non-rails environments

--- lib/activerecord-postgres-hstore.rb
+++ lib/activerecord-postgres-hstore.rb
@@ -1,4 +1,6 @@
-require "activerecord-postgres-hstore/railties"
+if defined? Rails
+  require "activerecord-postgres-hstore/railties"
+end
 require "activerecord-postgres-hstore/string"
 require "activerecord-postgres-hstore/hash"
 require "activerecord-postgres-hstore/coder"
