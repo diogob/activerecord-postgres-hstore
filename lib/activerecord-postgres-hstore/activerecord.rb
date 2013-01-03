87b7be2fbd4c802b3bf4dbc14e5e17d9c4e86d0e
Diogo Biazus <diogob@gmail.com>
2013-01-03 17:42:45 -0200

    fixed indentation

--- lib/activerecord-postgres-hstore/activerecord.rb
+++ lib/activerecord-postgres-hstore/activerecord.rb
@@ -67,28 +67,28 @@ module ActiveRecord
     end
 
     if defined? Rails and Rails.version < '3.1.0'
-    # This method is replaced for Rails 3 compatibility.
-    # All I do is add the condition when the field is a hash that converts the value
-    # to hstore format.
-    # IMHO this should be delegated to the column, so it won't be necessary to rewrite all
-    # this method.
-    def arel_attributes_values(include_primary_key = true, include_readonly_attributes = true, attribute_names = @attributes.keys)
-      attrs = {}
-      attribute_names.each do |name|
-        if (column = column_for_attribute(name)) && (include_primary_key || !column.primary)
-          if include_readonly_attributes || (!include_readonly_attributes && !self.class.readonly_attributes.include?(name))
-            value = read_attribute(name)
-            if self.class.columns_hash[name].type == :hstore && value && value.is_a?(Hash)
-              value = value.to_hstore # Done!
-            elsif value && self.class.serialized_attributes.has_key?(name) && (value.acts_like?(:date) || value.acts_like?(:time) || value.is_a?(Hash) || value.is_a?(Array))
-              value = value.to_yaml
+      # This method is replaced for Rails 3 compatibility.
+      # All I do is add the condition when the field is a hash that converts the value
+      # to hstore format.
+      # IMHO this should be delegated to the column, so it won't be necessary to rewrite all
+      # this method.
+      def arel_attributes_values(include_primary_key = true, include_readonly_attributes = true, attribute_names = @attributes.keys)
+        attrs = {}
+        attribute_names.each do |name|
+          if (column = column_for_attribute(name)) && (include_primary_key || !column.primary)
+            if include_readonly_attributes || (!include_readonly_attributes && !self.class.readonly_attributes.include?(name))
+              value = read_attribute(name)
+              if self.class.columns_hash[name].type == :hstore && value && value.is_a?(Hash)
+                value = value.to_hstore # Done!
+              elsif value && self.class.serialized_attributes.has_key?(name) && (value.acts_like?(:date) || value.acts_like?(:time) || value.is_a?(Hash) || value.is_a?(Array))
+                value = value.to_yaml
+              end
+              attrs[self.class.arel_table[name]] = value
             end
-            attrs[self.class.arel_table[name]] = value
           end
         end
+        attrs
       end
-      attrs
-    end
     end
 
   end
