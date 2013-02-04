fb59b1cd2daf8b499e2bf108d1ae5d89b2379426
Diogo Biazus <diogob@gmail.com>
2013-02-04 11:13:18 -0200

    rewrote coder specs for clarity

--- spec/activerecord-coders-hstore_spec.rb
+++ spec/activerecord-coders-hstore_spec.rb
@@ -1,27 +1,67 @@
 require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 
-describe "ActiverecordCodersHstore" do
-  it 'should load nil' do
-    ActiveRecord::Coders::Hstore.load(nil).should be_nil
+describe ActiveRecord::Coders::Hstore do
+  describe "#load" do
+    subject{ ActiveRecord::Coders::Hstore.new.load(value) }
+
+    context 'when value is nil and we have a default in the constructor' do
+      subject{ ActiveRecord::Coders::Hstore.new({'a'=>'a'}).load(nil) }
+      it{ should eql({'a'=>'a'}) }
+    end
+
+    context 'when value is nil' do
+      let(:value){ nil }
+      it { should be_nil }
+    end
+
+    context 'when value is a hstore' do
+      let(:value){ "a=>a" }
+      it{ should eql({ 'a' => 'a' }) }
+    end
   end
 
-  it 'should load an hstore' do
-    ActiveRecord::Coders::Hstore.load("a=>a").should == { 'a' => 'a' }
+  describe "#dump" do
+    subject{ ActiveRecord::Coders::Hstore.new.dump(value) }
+
+    context 'when value is nil and we have a default in the constructor' do
+      subject{ ActiveRecord::Coders::Hstore.new({'a'=>'a'}).dump(nil) }
+      it{ should eql({'a'=>'a'}.to_hstore) }
+    end
+
+    context 'when value is nil' do
+      let(:value){ nil }
+      it{ should be_nil }
+    end
+
+    context "when value is an hstore" do
+      let(:value){ {'a' => 'a'} }
+      it{ should eql(value.to_hstore) }
+    end
   end
 
-  it 'should dump an hstore' do
-    ActiveRecord::Coders::Hstore.dump({'a'=>'a'}).should == {'a'=>'a'}.to_hstore
+  describe ".load" do
+    before do
+      @parameter = 'b=>b'
+      instance = double("coder instance")
+      instance.should_receive(:load).with(@parameter)
+      ActiveRecord::Coders::Hstore.should_receive(:new).and_return(instance)
+    end
+
+    it("should instantiate and call load") do
+      ActiveRecord::Coders::Hstore.load(@parameter)
+    end
   end
 
-  it 'should dump nil' do
-    ActiveRecord::Coders::Hstore.dump(nil).should be_nil
-  end
+  describe ".dump" do
+    before do
+      @parameter = {'b' => 'b'}
+      instance = double("coder instance")
+      instance.should_receive(:dump).with(@parameter)
+      ActiveRecord::Coders::Hstore.should_receive(:new).and_return(instance)
+    end
 
-  it 'should dump the default given nil' do
-    ActiveRecord::Coders::Hstore.new({'a'=>'a'}).dump(nil).should == {'a'=>'a'}.to_hstore
-  end
-
-  it 'should load the default given nil' do
-    ActiveRecord::Coders::Hstore.new({'a'=>'a'}).load(nil).should eql({'a'=>'a'})
+    it("should instantiate and call load") do
+      ActiveRecord::Coders::Hstore.dump(@parameter)
+    end
   end
 end
